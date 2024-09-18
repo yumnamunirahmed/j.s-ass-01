ca6d37d3acc345a2a36eba3923bfa99e759c3c5d
yumnamunirahmed <yumnamunirahmed@gmail.com>
2024-09-18 15:37:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+    //  QUESTION NO:01 
+    alert("Yumna Munir Ahmed");
+    //  QUESTION NO:02
+    var na="Yumna Munir Ahmed"
+    console.log("My name is" + " " + na);
+        // QUESTION NO:03 
+        var a = 4
+        var b = 2
+        console.log(a++ + --b - --a + b++ + a + b)
+            // question no:04
+            var naa = prompt("You here for joining IT course?");
+            var age = prompt("How old are you?");
+            if( naa === "yes","Yes","y","Y","YES",age>=18){
+                alert("you are able to join web-designing course")
+             } else if (age<=17){
+                alert("you are able to join graphic-designing course")
+            } 
+            
+            
